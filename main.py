5292507bf8481015753c24037caca32411d6b515
Lukas Kollarovic <63819829+luuk00101@users.noreply.github.com>
2024-10-20 23:26:01 +0200

    Add sprite groups

--- main.py
+++ main.py
@@ -8,22 +8,37 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # Initilize the "control pobjects"
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    # Initilize the clock and delta time (for proper rendering)
     clock = pygame.time.Clock()
     dt = 0
 
+    # Main game loop
     while True:
+        # Allows to actually interact and exit the game
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        # Fill the background
         screen.fill("#000000")
-        player.update(dt)
-        player.draw(screen)
+
+        # Update and draw the sprites
+        for sprite in updatable:
+            sprite.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
+
+        # Actually render the screen (limit 60 FPS)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
