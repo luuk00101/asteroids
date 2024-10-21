607028dff878912638e8619f65d815dee4028a06
Lukas Kollarovic <63819829+luuk00101@users.noreply.github.com>
2024-10-21 07:39:17 +0200

    Add astreroid 'infra'

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -14,9 +16,14 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    ateroid_field = AsteroidField()
 
     # Initilize the clock and delta time (for proper rendering)
     clock = pygame.time.Clock()
